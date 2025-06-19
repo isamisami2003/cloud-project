80211a3fed92bc9c2cc9d288def35fb49aefb1dc
Ismail sami Ibrahim <43090518+isamisami2003@users.noreply.github.com>
2025-06-19 11:08:23 +0300

    Update documentController.js

--- controllers/documentController.js
+++ controllers/documentController.js
@@ -286,13 +286,5 @@ const getStats = async (req, res) => {
   }
 };
 
-const sort = async (req,res) => {
-  try {
-    const docs = await DocumentModel.find({}).sort({ title: 1 }); 
-    res.json(docs);
-  } catch (err) {
-    res.status(500).json({ message: 'wrong to get documents' });
-  }
-}
 
-module.exports = { uploadDocument, getAllDocuments, searchDocuments, classifyDocuments, getStats, sort}
+module.exports = { uploadDocument, getAllDocuments, searchDocuments, classifyDocuments, getStats}
