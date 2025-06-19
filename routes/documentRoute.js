f450d8f170f2789951e57753bb2fe6978a7ce2e9
Ismail sami Ibrahim <43090518+isamisami2003@users.noreply.github.com>
2025-06-19 11:24:13 +0300

    Update documentRoute.js

--- routes/documentRoute.js
+++ routes/documentRoute.js
@@ -6,6 +6,7 @@ const {
   searchDocuments,
   classifyDocuments,
   getStats,
+  sort
 } = require("../controllers/documentController");
 
 const router = express.Router();
@@ -16,6 +17,6 @@ router.get("/", getAllDocuments);
 router.get("/search", searchDocuments);
 router.post("/classify", classifyDocuments);
 router.get("/stats", getStats);
-
+router.get("/sort", sort);
 
 module.exports = router;
