feee4a9c7fd4910ad1b5bf9dc86ad3b3408cc2af
Ismail sami Ibrahim <43090518+isamisami2003@users.noreply.github.com>
2025-06-19 10:15:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const connectDB= require('./config/db')
 const documentRoute = require('./routes/documentRoute');
+const path = require('path');
+
 
 const port = 3000;
 
@@ -13,6 +15,9 @@ connectDB();
 
 app.use("/api/documents", documentRoute);
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 app.listen(port, ()=>{
   console.log(`server is running now on port ${port}`);
